dc000b3f53dcfb8cec33e4fa4223e65ef808db5f
Samir <yourchenko@gmail.com>
2012-08-06 16:57:13 +0100

    New : Improved design and fonts and structure of the site

--- client/FirstMeteorApp.js
+++ client/FirstMeteorApp.js
@@ -63,13 +63,18 @@ Template.artists.artists = function() {
 	return Artists.find({}, {sort: {name: 1}});
 };
 
-Template.artists.selected = function() {
-	return Session.equals('selected_artist', this._id) ? 'selected' : '';
-};
-
 Template.artists.count = function () {
 	return Artists.find().count();
 }
+
+Template.artists.albumlist = function(artist_id){
+	albums = Albums.find({artist_id: artist_id});
+	if(albums.count() === 0) {
+		return false;
+	}
+	return albums;
+};
+
 
 // Template.artists.events = {
 // 	'click': function () = {
@@ -79,9 +84,17 @@ Template.artists.count = function () {
 // 	}
 // };
 
-Template.artist_item.selected = function() {
-	return Session.equals('selected_artist', this._id) ? 'selected' : '';
-};
+// Template.artist_item.selected = function() {
+// 	return Session.equals('selected_artist', this._id) ? 'selected' : '';
+// };
+
+Template.album_item.songlist = function(album_id){
+	songs = Songs.find({album_id: album_id});
+	if(songs.count() === 0){
+		return false;
+	}
+	return songs;
+}
 
 
 Template.artist_item.events = {
@@ -98,99 +111,99 @@ Template.artist_item.events = {
 
 /////////////////////  Albums  /////////////////////////////
 //Display the correct album depending on the selected artist
-Template.albums.albums = function() {
-	var artist_id = Session.get("selected_artist");
-	if(!artist_id){
-		return Albums.find({}, {sort: {artist_id: 1, name: 2}});
-	}else{
-		return Albums.find({artist_id: artist_id}, {sort: {name: 1}});
-	}
-};
+// Template.albums.albums = function() {
+// 	var artist_id = Session.get("selected_artist");
+// 	if(!artist_id){
+// 		return Albums.find({}, {sort: {artist_id: 1, name: 2}});
+// 	}else{
+// 		return Albums.find({artist_id: artist_id}, {sort: {name: 1}});
+// 	}
+// };
 
-Template.albums.count = function () {
-	var artist_id = Session.get("selected_artist");
-	if(!artist_id){
-		return Albums.find().count();
-	}else{
-		return Albums.find({artist_id: artist_id}).count();
-	}
-}
+// Template.albums.count = function () {
+// 	var artist_id = Session.get("selected_artist");
+// 	if(!artist_id){
+// 		return Albums.find().count();
+// 	}else{
+// 		return Albums.find({artist_id: artist_id}).count();
+// 	}
+// }
 
-Template.albums.selected = function() {
-	return Session.equals('album_id', this._id) ? 'selected' : '';
-};
-
-
-Template.album_item.selected = function() {
-	return Session.equals('selected_album', this._id) ? 'selected' : '';
-};
+// Template.albums.selected = function() {
+// 	return Session.equals('album_id', this._id) ? 'selected' : '';
+// };
 
 
-//When album selected, we auto select the correspondent artist
-Template.album_item.events = {
-	'click': function () {
-		Session.set("selected_album", this._id);
-		Session.set("selected_artist", this.artist_id);
-	}
-};
+// Template.album_item.selected = function() {
+// 	return Session.equals('selected_album', this._id) ? 'selected' : '';
+// };
 
 
-/////////////////////////  Songs  /////////////////////////////
-// If selected_album, we display the correspondent songs
-// If selected artist, we display the correspondent songs 
-//
-Template.songs.songs = function() {
-	var album_id = Session.get("selected_album");
-	var artist_id = Session.get("selected_artist");
+// //When album selected, we auto select the correspondent artist
+// Template.album_item.events = {
+// 	'click': function () {
+// 		Session.set("selected_album", this._id);
+// 		Session.set("selected_artist", this.artist_id);
+// 	}
+// };
 
-	if (!artist_id && !album_id){
-		return Songs.find({}, {sort: {album_id: 1, track: 2}});
-	}else if(artist_id && !album_id) {//we fetch the correspondent albums
-		// var albums = Albums.find({artist_id: artist_id}, {fields: {_id: 1}}).fetch();//array of Ids FETCH & FIELDS DOESN4T WORK
-		var albums = Albums.find({artist_id: artist_id});
-		var albums_ids = [];
-		var i = 0;
-		albums.forEach(function (album){
-			albums_ids.push(album._id);
-		});
-		return Songs.find({album_id: {$in: albums_ids}}, {sort: {album_id: 1, track: 2}});
-	}else if(artist_id && album_id){
-		return Songs.find({album_id: album_id}, {sort: {track: 1}});
-	}
-};
 
-Template.songs.count = function () {
-	var album_id = Session.get("selected_album");
-	var artist_id = Session.get("selected_artist");
+// /////////////////////////  Songs  /////////////////////////////
+// // If selected_album, we display the correspondent songs
+// // If selected artist, we display the correspondent songs 
+// //
+// Template.songs.songs = function() {
+// 	var album_id = Session.get("selected_album");
+// 	var artist_id = Session.get("selected_artist");
 
-	if (!artist_id && !album_id){
-		return Songs.find().count();
-	}else if(artist_id && !album_id) {//we fetch the correspondent albums
-		// var albums = Albums.find({artist_id: artist_id}, {fields: {_id: 1}}).fetch();//array of Ids FETCH & FIELDS DOESN4T WORK
-		var albums = Albums.find({artist_id: artist_id});
-		var albums_ids = [];
-		var i = 0;
-		albums.forEach(function (album){
-			albums_ids.push(album._id);
-		});
-		return Songs.find({album_id: {$in: albums_ids}}).count();
-	}else if(artist_id && album_id){
-		return Songs.find({album_id: album_id}).count();
-	}
-}
+// 	if (!artist_id && !album_id){
+// 		return Songs.find({}, {sort: {album_id: 1, track: 2}});
+// 	}else if(artist_id && !album_id) {//we fetch the correspondent albums
+// 		// var albums = Albums.find({artist_id: artist_id}, {fields: {_id: 1}}).fetch();//array of Ids FETCH & FIELDS DOESN4T WORK
+// 		var albums = Albums.find({artist_id: artist_id});
+// 		var albums_ids = [];
+// 		var i = 0;
+// 		albums.forEach(function (album){
+// 			albums_ids.push(album._id);
+// 		});
+// 		return Songs.find({album_id: {$in: albums_ids}}, {sort: {album_id: 1, track: 2}});
+// 	}else if(artist_id && album_id){
+// 		return Songs.find({album_id: album_id}, {sort: {track: 1}});
+// 	}
+// };
 
-//Click on a song => selection of the correspondent artist/album
-Template.song_item.events = {
-	'click': function () {
-		Session.set("selected_song", this._id);
-		Session.set("selected_album", this.album_id);
+// Template.songs.count = function () {
+// 	var album_id = Session.get("selected_album");
+// 	var artist_id = Session.get("selected_artist");
 
-		var album = Albums.findOne({_id: this.album_id});
-		Session.set("selected_artist", album.artist_id);
+// 	if (!artist_id && !album_id){
+// 		return Songs.find().count();
+// 	}else if(artist_id && !album_id) {//we fetch the correspondent albums
+// 		// var albums = Albums.find({artist_id: artist_id}, {fields: {_id: 1}}).fetch();//array of Ids FETCH & FIELDS DOESN4T WORK
+// 		var albums = Albums.find({artist_id: artist_id});
+// 		var albums_ids = [];
+// 		var i = 0;
+// 		albums.forEach(function (album){
+// 			albums_ids.push(album._id);
+// 		});
+// 		return Songs.find({album_id: {$in: albums_ids}}).count();
+// 	}else if(artist_id && album_id){
+// 		return Songs.find({album_id: album_id}).count();
+// 	}
+// }
 
-	}
-};
+// //Click on a song => selection of the correspondent artist/album
+// Template.song_item.events = {
+// 	'click': function () {
+// 		Session.set("selected_song", this._id);
+// 		Session.set("selected_album", this.album_id);
 
-Template.song_item.selected = function() {
-	return Session.equals('selected_song', this._id) ? 'selected' : '';
-};
+// 		var album = Albums.findOne({_id: this.album_id});
+// 		Session.set("selected_artist", album.artist_id);
+
+// 	}
+// };
+
+// Template.song_item.selected = function() {
+// 	return Session.equals('selected_song', this._id) ? 'selected' : '';
+// };
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -5,68 +5,81 @@ Meteor.startup(function(){
 		var data = [
 			{
 				artist: "Orelsan",
-				name: "Le chant des sirènes",
-				songs: [
-					["Raelsan",  "Raelsan.mp3"],
-					["Le chant des sirènes", "Le Chant Des Sirènes.mp3"],
-					["Plus rien ne m'étonne", "Plus Rien Ne M_étonne.mp3"],
-					["Mauvaise idée", "Mauvaise Idée.mp3"],
-					["Double vie", "Double Vie.mp3"],
-					["Finir Mal", "Finir Mal.mp3"],
-					["Si seul", "Si Seul.mp3"],				
-					["Des trous dans les tête", "Des Trous Dans La Tête.mp3"],
-					["La petite marchande de clés", "09 La Petite Marchande De Portes-Clefs.mp3"],
-					["La terre est ronde", "La Terre Est Ronde.mp3"],
-					["1990", "1990.mp3"],
-					["2010", "2010.mp3"],
-					["La morale", "La Morale.mp3"],
-					["Ils sont cools", "Ils Sont Cools.mp3"],
-					["Suicide social", "Suicide Social.mp3"],
-					["Elle viendra quant même", "Elle Viendra Quand Même.mp3"]
-				]
+				albums: [{
+					name: "Le chant des sirènes",
+					songs: [
+						["Raelsan",  "Raelsan.mp3"],
+						["Le chant des sirènes", "Le Chant Des Sirènes.mp3"],
+						["Plus rien ne m'étonne", "Plus Rien Ne M_étonne.mp3"],
+						["Mauvaise idée", "Mauvaise Idée.mp3"],
+						["Double vie", "Double Vie.mp3"],
+						["Finir Mal", "Finir Mal.mp3"],
+						["Si seul", "Si Seul.mp3"],				
+						["Des trous dans les tête", "Des Trous Dans La Tête.mp3"],
+						["La petite marchande de clés", "09 La Petite Marchande De Portes-Clefs.mp3"],
+						["La terre est ronde", "La Terre Est Ronde.mp3"],
+						["1990", "1990.mp3"],
+						["2010", "2010.mp3"],
+						["La morale", "La Morale.mp3"],
+						["Ils sont cools", "Ils Sont Cools.mp3"],
+						["Suicide social", "Suicide Social.mp3"],
+						["Elle viendra quant même", "Elle Viendra Quand Même.mp3"]
+					]
+				},
+				{
+					name: "Perdu d'avance",
+					songs: [
+						["Changement",  "changement.mp3"],
+						["no life", "no life.mp3"],
+						["Soirée ratée", "Soirée ratée.mp3"],
+						["Différent", "Mauvaise Idée.mp3"],
+						["Peur de l'échec", "Peur de l'échec.mp3"]
+					]
+				}]
 			},
 			{
 				artist: "The xx",
-				name: "xx",
-				songs: [
-						["Intro", "Intro.mp3"],
-						["VCR", "Vcr.mp3"],
-						["Crystalised", "Crystalised.mp3"],
-						["Islands", "Islands.mp3"],
-						["Heard skipped at beat", "Heart Skipped A Beat.mp3"],
-						["Fantasy", "Fantasy.mp3"],
-						["Shelter", "Shelter.mp3"],
-						["Basic space", "Basic Space.mp3"],
-						["Infinity", "Infinity.mp3"],
-						["Night time", "Night Time.mp3"],
-						["Stars", "Stars.mp3"]
-					]
+				albums: [{
+					name: "xx",
+					songs: [
+							["Intro", "Intro.mp3"],
+							["VCR", "Vcr.mp3"],
+							["Crystalised", "Crystalised.mp3"],
+							["Islands", "Islands.mp3"],
+							["Heard skipped at beat", "Heart Skipped A Beat.mp3"],
+							["Fantasy", "Fantasy.mp3"],
+							["Shelter", "Shelter.mp3"],
+							["Basic space", "Basic Space.mp3"],
+							["Infinity", "Infinity.mp3"],
+							["Night time", "Night Time.mp3"],
+							["Stars", "Stars.mp3"]
+						]
+				},{
+					name: "Angels",
+					songs: [
+							["Angels", "Angels.mp3"]
+						]
+
+				}]
 			}
 		];
 		
-		//var timestamp = (new Date()).getTime();
-
-		// var artist_id = Artists.insert({name: data[0].artist});
-		// var album_id = Albums.insert({artist_id: artist_id, name: data[0].name});
-
-	 	// for (var j = 0; j < data[0].songs.length; j++) {
-	 	//			var info = data[0].songs[j];
-	 	//     		Songs.insert({album_id: album_id, title: info});
-	 	//    }
 
 	    for (var i = 0; i < data.length; i++) {
 	       	//Create the Artists
 	    	var artist_id = Artists.insert({name: data[i].artist});
 
 	    	//Create the album
-	    	var album_id = Albums.insert({artist_id: artist_id, name: data[i].name});
+	    	for (var a=0; a<data[i].albums.length; a++){
+	    		var album_id = Albums.insert({artist_id: artist_id, name: data[i].albums[a].name});
 
-
-	    	//Add the songs for the albums
-	    	for (var j = 0; j < data[i].songs.length; j++) {
-	    		var info = data[i].songs[j];
-	    		Songs.insert({album_id: album_id, title: info[0], track: j, path: info[1]});
+		    	//Add the songs for the albums
+		    	for (var j = 0; j < data[i].albums[a].songs.length; j++) {
+		    		var info = data[i].albums[a].songs[j];
+		    		Songs.insert({album_id: album_id, title: info[0], track: j, path: info[1]});
+		        }
 	        }
 	    }
-	}
+
+	}//end if
 });
